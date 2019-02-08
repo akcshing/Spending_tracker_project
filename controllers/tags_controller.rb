f2905b7d9b6353b0e2e027d36dc612d5f0ca9105
Alexkcshing <alexkcshing@gmail.com>
2019-02-08 19:23:50 +0000

    add working tags and transactions index route

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -5,3 +5,8 @@ require_relative("../models/merchant.rb")
 require_relative("../models/transaction.rb")
 also_reload("../models/*")
 require("pry")
+
+get "/tags" do
+  @tags = Tag.all()
+  erb (:"tags/index")
+end
--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -5,3 +5,8 @@ require_relative("../models/merchant.rb")
 require_relative("../models/transaction.rb")
 also_reload("../models/*")
 require("pry")
+
+get "/transactions" do
+  @transactions = Transaction.all()
+  erb (:"transactions/index")
+end
