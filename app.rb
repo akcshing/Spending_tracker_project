5f6cd92bfad61d8d11de69ff64e15bc942442c1b
Alexkcshing <alexkcshing@gmail.com>
2019-02-08 19:14:48 +0000

    add working merchant/index route with table

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require("sinatra")
 require("sinatra/contrib/all")
-# require_relative("/controllers/tags_controller.rb")
-# require_relative("/controllers/merchants_controller.rb")
-# require_relative("/controllers/transactions_controller.rb")
+require_relative("controllers/tags_controller.rb")
+require_relative("controllers/merchants_controller.rb")
+require_relative("controllers/transactions_controller.rb")
 also_reload("/models/*")
 require("pry")
 
 get "/" do
-  erb (:"index")
+  erb (:index)
 end
