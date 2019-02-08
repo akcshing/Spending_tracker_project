0a4b5cbac77f9dc4486988cfd2baddb44bb8ef1f
Alexkcshing <alexkcshing@gmail.com>
2019-02-08 18:57:39 +0000

    add app controller index route

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -1,8 +1,8 @@
 require("sinatra")
 require("sinatra/contrib/all")
-require_relative("../models/tag.rb")
-require_relative("../models/merchant.rb")
-require_relative("../models/transaction.rb")
+# require_relative("../models/tag.rb")
+# require_relative("../models/merchant.rb")
+# require_relative("../models/transaction.rb")
 also_reload("../models/*")
 require("pry")
 
