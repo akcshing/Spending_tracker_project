37c2aa227f33221e9db6d70553af1863d6fb11e0
Alexkcshing <alexkcshing@gmail.com>
2019-02-09 12:59:43 +0000

    add further scoping to setup data and pass tests

--- models/specs/transaction_sepc.rb
+++ models/specs/transaction_sepc.rb
@@ -18,20 +18,20 @@ class TestTransaction < MiniTest::Test
     @merchant_2 = Merchant.new("name"=>"UNIQLO")
     @merchant_2.save()
 
-    @transaction_1 = Transaction.new("amount" => 6.80, "tag_id"=>tag_1.id, "merchant_id"=>merchant_1.id)
+    @transaction_1 = Transaction.new("amount" => 6.80, "tag_id"=>@tag_1.id, "merchant_id"=>@merchant_1.id)
     @transaction_1.save()
 
-    @transaction_2 = Transaction.new("amount" => 5.40, "tag_id"=>tag_1.id, "merchant_id"=>merchant_1.id)
+    @transaction_2 = Transaction.new("amount" => 5.40, "tag_id"=>@tag_1.id, "merchant_id"=>@merchant_1.id)
     @transaction_2.save()
 
-    @transaction_3 = Transaction.new("amount" => 7.80, "tag_id"=>tag_2.id, "merchant_id"=>merchant_2.id)
+    @transaction_3 = Transaction.new("amount" => 7.80, "tag_id"=>@tag_2.id, "merchant_id"=>@merchant_2.id)
     @transaction_3.save()
   end
 
   def teardown
-    # Tag.delete_all
-    # Merchant.delete_all
-    # Transaction.delete_all
+    Tag.delete_all
+    Merchant.delete_all
+    Transaction.delete_all
   end
 
   def test_amount_total
@@ -39,7 +39,7 @@ class TestTransaction < MiniTest::Test
   end
 
   def test_get_tag
-    assert_equal(2, @transaction_3.tag.id) # get trans 3 from table, tag method, id
+    assert_equal(@tag_2.id, @transaction_3.tag.id) # get trans 3 from table, tag method, id
     assert_equal("Clothes", @transaction_3.tag.name)
   end
 
